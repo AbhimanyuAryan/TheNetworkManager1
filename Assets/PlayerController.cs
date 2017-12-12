c93f85c70de31de7f81993678bb83772e9492260
DESKTOP-2LV9GDO\GK_Desk_07 <abhimanyu.chat@gmail.com>
2017-12-12 08:43:21 +0530

    HUD, Manager, Idenity and stuff, Also NetworkBehaviour is LocalPlayer

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿//adapted from example script available at
+//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+
+	// Update is called once per frame
+	void Update () {
+	
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+	}
+}
---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; 
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer){
+			GetComponent<PlayerController>().enabled = true;
+		}else{
+			GetComponent<PlayerController>().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
